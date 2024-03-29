ecea110f3389f60188dc434595e1d77dd9ffae8a
adamd <adam.d.hinton@gmail.com>
2024-03-28 21:37:03 -0400

    Working solution (mostly)

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,64 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        BankCustomer bob = new BankCustomer("Bob", new ArrayList<>(List.of(
+                new BankAccount(BankAccount.BankAccountType.CHECKING, 1000),
+                new BankAccount(BankAccount.BankAccountType.SAVINGS, 4000)
+        )));
+
+        System.out.println(bob.toString());
+
     }
 }
+
+class BankAccount{
+    enum BankAccountType{
+        SAVINGS, CHECKING
+    }
+
+    private final BankAccountType bankAccountType;
+    private final double balance;
+
+    public BankAccount(BankAccountType bankAccountType, double balance) {
+        this.bankAccountType = bankAccountType;
+        this.balance = balance;
+    }
+}
+
+class BankCustomer {
+
+    static int CUSTOMER_ID = 1;
+    private final String customerName;
+    private final int customerID;
+    private final ArrayList<BankAccount> bankAccounts;
+
+    public BankCustomer(String customerName, ArrayList<BankAccount> bankAccounts) {
+        this.customerName = customerName;
+        this.customerID = CUSTOMER_ID;
+        this.bankAccounts = bankAccounts == null ? null : Arrays.copyOf(bankAccounts);
+        CUSTOMER_ID ++;
+    }
+}
+
+
+
+// make class BankAccount
+// BA fields:
+    // type: Savings or Checking
+    // Balance: initial dollar amount
+
+// BankCustomer class
+    // fields:
+        // customerName
+        // customerID
+        // List of accounts
+
+// Create a couple Customer instances and confirm you can't change a Customer's data at all after it's initialized
+
+// create a subclass of BC and confirm the subclass can't tamper with customer's data either
